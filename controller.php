baaa7dfb8433cbb6cca56fc47ac50dc0c3c0ad8e
Shreenath3323 <silakshreenath@gmail.com>
2023-04-15 23:00:05 +0530

    removed pagination

--- controller.php
+++ controller.php
@@ -1,31 +1,31 @@
 <?php
-    $file_home="home.php";      // done
-    $file_index="index.php";    //done
-    $file_login="login.php";    //done
-    $file_logout="logout.php";  //done
-    $file_user_info="user_info.php";    //done
-    $file_userRegisterInfo="userRegisterInfo.php";  //done
+    $file_home="home.php";      
+    $file_index="index.php";    
+    $file_login="login.php";    
+    $file_logout="logout.php";  
+    $file_user_info="user_info.php";    
+    $file_userRegisterInfo="userRegisterInfo.php";  
 
     //api folder
 
-    $file_CheckRegistration="CheckRegistration.php";    //done
-    $file_CreatePayment="CreatePayment.php";    //un  //done
-    $file_GetUserDetails="GetUserDetails.php";  //un    //done
-    $file_GetUserRegistrationDetail="GetUserRegistrationDetail.php";    //done
-    $file_RegisterEvent="RegisterEvent.php";    //done
-    $file_SearchByEnrollment="SearchByEnrollment.php";  //done
-    $file_SearchByEventName="SearchByEventName.php";    //done
-    $file_SearchByFirstName="SearchByFirstName.php";    //done
-    $file_SearchByLastName="SearchByLastName.php";  //done
-    $file_UpdatePayment="UpdatePayment.php";    //un  //done
-    $file_VerifyUser="VerifyUser.php";  //un  //done
+    $file_CheckRegistration="CheckRegistration.php";    
+    $file_CreatePayment="CreatePayment.php";      
+    $file_GetUserDetails="GetUserDetails.php";      
+    $file_GetUserRegistrationDetail="GetUserRegistrationDetail.php";    
+    $file_RegisterEvent="RegisterEvent.php";    
+    $file_SearchByEnrollment="SearchByEnrollment.php";  
+    $file_SearchByEventName="SearchByEventName.php";    
+    $file_SearchByFirstName="SearchByFirstName.php";    
+    $file_SearchByLastName="SearchByLastName.php";  
+    $file_UpdatePayment="UpdatePayment.php";      
+    $file_VerifyUser="VerifyUser.php";    
 
     //event folder
 
-    $file_event_addform="event_addform.php";    //done
-    $file_event_view="event_view.php";      //done
+    $file_event_addform="event_addform.php";    
+    $file_event_view="event_view.php";      
 
     //registration folder
 
-    $file_EventRegistration="EventRegistration.php";  //check
+    $file_EventRegistration="EventRegistration.php";  
 ?>
